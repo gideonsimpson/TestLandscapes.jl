faf119fae1473c6b519c4b82298a2a0882c06b05
Gideon Simpson <gideon.simpson@gmail.com>
2019-05-29 19:53:38 -0400

    added over code from personal repo for 1D and 2D potentials

---
+++ src/TestPotentials1D.jl
@@ -0,0 +1,20 @@
+module TestPotentials1D
+# a module of simple potentials for testing optimization and sampling methods
+
+# classic symmetric doublewell potential
+function SymmetricDoubleWell(x)
+    return (x^2 -1)^2;
+end
+
+# asymmetric  double well potential requires |δ|< 1 for multiple minima
+function ASymmetricDoubleWell(x;δ=0.5)
+    return x^4 - 1.5 * x^2 - δ * x;
+end
+
+# Fat-skinny double well of degree 10. This introduces entropic effects in
+# dimension 1
+function FatSkinnyDoubleWell10(x)
+    return ((8-5*x)^8 * (2+5*x)^2)/(2^26);
+end
+
+end # end module
---
+++ src/TestPotentials2D.jl
@@ -0,0 +1,42 @@
+module TestLandScapes2D
+# a module of simple potentials for testing optimization and sampling methods in
+# 2D
+
+# Entropic Switching potential has three local
+# minima and is symmetric about x = 0
+function EntropicSwitch(x)
+    return (3 * exp(-x[1]^2 - (x[2]-1/3)^2)
+     - 3 * exp(-x[1]^2 - (x[2]-5/3)^2)
+     - 5 * exp(-(x[1]-1)^2 - x[2]^2)
+     - 5 * exp(-(x[1]+1)^2 - x[2]^2)
+     + 1/5 * x[1]^4 + 1/5 * (x[2]-1/3)^4);
+end
+
+# Double well potential in 2D with two, symmetric channels joining them
+function SymmetricTwoChannel(x)
+    return 1/6 * (4 * (1-x[1]^2-x[2]^2)^2 + 2 *(x[1]^2-2)^2
+    + ((x[1]+x[2])^2 - 1 )^2+ ((x[1]-x[2])^2 - 1 )^2);
+end
+
+# Mueller potential
+# Three distinct minima and highy asymmetric
+function Mueller(x)
+
+    aa = Float64[-1, -1, -6.5, 0.7];
+    bb = Float64[0., 0., 11., 0.6];
+    cc = Float64[-10., -10., -6.5, 0.7];
+    AA = Float64[-200., -100., -170., 15.];
+    XX = Float64[1., 0., -0.5, -1.];
+    YY = Float64[0., 0.5, 1.5, 1.];
+
+    return ( AA[1]*exp(aa[1]*(x[1]-XX[1])^2+bb[1]*(x[1]-XX[1])*(x[2]-YY[1])+cc[1]*(x[2]-YY[1])^2)
+                 +AA[2]*exp(aa[2]*(x[1]-XX[2])^2+bb[2]*(x[1]-XX[2])*(x[2]-YY[2])+cc[2]*(x[2]-YY[2])^2)
+                 +AA[3]*exp(aa[3]*(x[1]-XX[3])^2+bb[3]*(x[1]-XX[3])*(x[2]-YY[3])+cc[3]*(x[2]-YY[3])^2)
+                 +AA[4]*exp(aa[4]*(x[1]-XX[4])^2+bb[4]*(x[1]-XX[4])*(x[2]-YY[4])+cc[4]*(x[2]-YY[4])^2));
+
+
+end
+
+
+
+end # module
